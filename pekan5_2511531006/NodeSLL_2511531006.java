b87ccd0aa27a13bb3bbd3086f358f05dd50a8e8f
lexi210919-dotcom <lexi210919@gmail.com>
2026-05-05 04:00:33 -0700

    Add files via upload

---
+++ pekan5_2511531006/NodeSLL_2511531006.java
@@ -0,0 +1,14 @@
+package pekan5_2511531006;
+
+public class NodeSLL_2511531006 {
+     // node bagian data
+	int data_2511531006;
+	// pointer ke node berikutnya
+	NodeSLL_2511531006 next_2511531006;
+	// Konstruktor menginisialisasi node dengan data
+	public NodeSLL_2511531006(int data_2511531006)
+	{
+		this.data_2511531006 = data_2511531006;
+		this.next_2511531006 = null;
+	}
+}
