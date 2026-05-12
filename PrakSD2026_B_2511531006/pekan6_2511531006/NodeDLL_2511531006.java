d50d99eba287960077022bc17db14c389c961ace
lexi210919-dotcom <lexi210919@gmail.com>
2026-05-11 21:24:51 -0700

    Add files via upload

---
+++ PrakSD2026_B_2511531006/pekan6_2511531006/NodeDLL_2511531006.java
@@ -0,0 +1,16 @@
+package pekan6_2511531006S;
+
+public class NodeDLL_2511531006 {
+	// mendefinisikan kelas Node
+	int data_2511531006; // data
+	NodeDLL_2511531006 next_2511531006; // pointer ke next kode
+	NodeDLL_2511531006 prev_2511531006; // pointer ke previous node
+	
+	// konstruktor 
+	public NodeDLL_2511531006(int data_2511531006) {
+		this.data_2511531006 = data_2511531006;
+		this.next_2511531006 = null;
+		this.prev_2511531006 = null;
+	}
+
+}
