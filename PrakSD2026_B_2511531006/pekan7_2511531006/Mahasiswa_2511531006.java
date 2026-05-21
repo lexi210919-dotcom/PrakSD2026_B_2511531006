5544390c132243c8add44f9b07af7bb1aeb7bc67
lexi210919-dotcom <lexi210919@gmail.com>
2026-05-21 08:43:23 -0700

    Add files via upload

---
+++ PrakSD2026_B_2511531006/pekan7_2511531006/Mahasiswa_2511531006.java
@@ -0,0 +1,27 @@
+package pekan7_2511531006;
+
+public class Mahasiswa_2511531006 {
+    private String nama_2511531006;
+    private String nim_2511531006;
+    private String prodi_2511531006;
+
+    public Mahasiswa_2511531006(String nama_2511531006, String nim_2511531006, String prodi_2511531006) {
+        this.nama_2511531006 = nama_2511531006;
+        this.nim_2511531006 = nim_2511531006;
+        this.prodi_2511531006 = prodi_2511531006;
+   
+  }
+
+    public String getNama_2511531006() {
+    	return nama_2511531006;
+    	}
+    
+    public String getNim_2511531006()  { 
+    	return nim_2511531006;
+    	}
+    
+    public String getProdi_2511531006(){
+    	return prodi_2511531006; 
+    	}
+}
+
